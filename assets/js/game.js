4797a51cb42fc831aa4ad6c19deab52ff980365c
Hz Donut <dstarrtrey@gmail.com>
2018-12-23 18:27:11 -0600

    adds DROIDS object

---
+++ assets/js/game.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    const DROIDS = {
+        bb8: {
+            name: "BB8 Astromech Droid",
+            hp: 110,
+            ap: 7,
+            cap: 7,
+            img: "../images/bb8.jpg"
+        },
+        c3po: {
+            name: "C-3PO Protocol Droid",
+            hp: 150,
+            ap: 13,
+            cap: 25,
+            img: "../images/c3po.jpg"
+        },
+        r2d2: {
+            name: "R2-D2 Astromech Droid",
+            hp: 180,
+            ap: 18,
+            cap: 35,
+            img: "../images/r2d2.jpg"
+        },
+        gonkDroid: {
+            name: "GNK (Gonk) Power Droid",
+            hp: 250,
+            ap: 50,
+            cap: 50,
+            img: "../images/gonk_droid.jpg"
+        },
+        mouseDroid: {
+            name: "MSE-6 (Mouse) Repair Droid",
+            hp: 98,
+            ap: 9,
+            cap: 3,
+            img: "../images/mouse_droid.jpg"
+        },
+        battleDroid: {
+            name: "B1 Battle Droid",
+            hp: 175,
+            ap: 19,
+            cap: 14,
+            img: "../images/battle_droid.jpg"
+        },
+        droideka: {
+            name: "Droideka Battle Droid",
+            hp: 200,
+            ap: 28,
+            cap: 39,
+            img: "../images/droideka.jpg"
+        }
+    }
+});
